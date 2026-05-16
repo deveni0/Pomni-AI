fad655caae87bd98fef5f19ef5cb18e0e15347b1
Aizen-55 <zbsksnbmkn33@gmail.com>
2026-05-17 00:51:14 +0300

    Update index.js
    تعديلات على بوت ايزن سوسكي

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '967781869314', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
   // Owner 1
-    { name: "VA", lid: "247579682029763@lid", jid: "972569311531@s.whatsapp.net" },
+    { name: "Aizen👑", lid: "967781869314@lid", jid: "967781869314@s.whatsapp.net" },
   // Owner 2
-    { name: "emam", lid: "221307316789354@lid", jid: "201144480436@s.whatsapp.net" },
+    { name: "emam", lid: "221307316789354@lid", jid: "967777815673@s.whatsapp.net" },
   // Owner 3
     { name: "Sukuna", jid: "201033024135@s.whatsapp.net", lid: "50414477168824@lid" },
   // Owner 4 
@@ -33,8 +33,8 @@ if (!global.db) {
 /* =========== Config ========== */
 const { config } = client;
 config.info = { 
-  nameBot: "♡ 𝙋𝙊𝙈𝙉𝙄 🎪 〈", 
-  nameChannel: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️", 
+  nameBot: "♡ pot Aizen👑  🎪〈", 
+  nameChannel: "انا عمك ي هطف 👑👑🕷️", 
   idChannel: "120363225356834044@newsletter",
   urls: {
     repo: "https://github.com/deveni0/Pomni-AI",
@@ -42,13 +42,13 @@ config.info = {
     channel: "https://whatsapp.com/channel/0029VaQim2bAu3aPsRVaDq3v"
   },
   copyright: { 
-    pack: 'ڤـ ـ VA ـ ـا', 
-    author: 'VA'
+    pack: 'Aizen Soskai👑', 
+    author: 'Aizen 👑'
   },
   images: [
-    "https://i.pinimg.com/originals/11/26/97/11269786cdb625c60213212aa66273a9.png",
-    "https://i.pinimg.com/originals/e2/21/20/e221203f319df949ee65585a657501a2.jpg",
-    "https://i.pinimg.com/originals/bb/77/0f/bb770fad66a634a6b3bf93e9c00bf4e5.jpg"
+    "https://images.gemini.googleusercontent.com/web/1000097751.png ",
+    "https://images.gemini.googleusercontent.com/web/1000097751.png ",
+    "https://images.gemini.googleusercontent.com/web/1000097751.png "
   ]
 };
 
